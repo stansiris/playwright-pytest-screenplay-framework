9b9609a1bf9341b436eeb98ada971b028c5307b2
stansiris <stan.siris@gmail.com>
2026-02-25 01:04:39 -0500

    Fixed formatting

--- screenplay_core/core/question.py
+++ screenplay_core/core/question.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from screenplay_core.core.actor import Actor
 
+
 class Question(ABC):
     """Base class for queries about the system under test."""
 
