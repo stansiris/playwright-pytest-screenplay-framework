351566769d655f13c850859c8e53eeebdbbe9658
stansiris <stan.siris@gmail.com>
2026-02-25 01:03:22 -0500

    Fixed the import order

--- screenplay_core/questions/is_visible.py
+++ screenplay_core/questions/is_visible.py
@@ -1,6 +1,6 @@
+from screenplay_core.core.actor import Actor
 from screenplay_core.core.question import Question
 from screenplay_core.core.target import Target
-from screenplay_core.core.actor import Actor
 
 
 class IsVisible(Question):
